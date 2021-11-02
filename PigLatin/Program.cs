b695447d6836d46af410aece3d651df7c68f2935
Madalina Varasteanu <madalinavarasteanu@rocketmortgage.com>
2021-11-01 23:37:45 -0400

    creating repo

---
+++ PigLatin/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace PigLatin
+{
+    public class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string userInput = GetInput("Please input a word or sentence to translate to pig Latin");
+
+        //    string translation = ToPigLatin(userInput);
+        //    Console.WriteLine(translation);
+        //}
+    }
+}
---
+++ PigLatin/TestPigLatin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PigLatin
+{
+   public class TestPigLatin
+    {
+        //public static string GetInput(string prompt)
+        //{
+        //    Console.WriteLine(prompt);
+        //    string input = Console.ReadLine().ToLower().Trim();
+        //    return input;
+        //}
+
+        [Theory]
+        [InlineData("apple", "appleway")]
+        [InlineData("heck", "eckhay")]
+        [InlineData("strong", "ongstray")]
+        [InlineData("tommy@email.com", "tommy@email.com")]
+        [InlineData("aardvark", "aardvarkway")]
+        [InlineData("Tommy", "onmytay")]
+        [InlineData("gym", "gym")]
+        [InlineData("apple joy gym tommy@email.com strong", "appleway oyjay gym tommy@email.com ongstray")]
+
+        public void TestPig(string input, string expected)
+        {
+            //Arrange
+            PigLatin pig = new PigLatin();
+
+            //act
+            string actual = pig.ToPigLatin(input);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+
+    }
+}
